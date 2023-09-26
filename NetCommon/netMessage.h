e726ace1bd8bfd2ab78657dd0e8abc8b580c4806
Jack Milo <mil21001248@btc.ac.uk>
2023-09-26 16:18:35 +0100

    Added to netMessage.h

--- NetCommon/netMessage.h
+++ NetCommon/netMessage.h
@@ -12,5 +12,25 @@ namespace olc
 			T id{};
 			uint32_t size = 0;
 		};
+
+		// Struct that represents the message
+		template <typename T>
+		struct message
+		{
+			message_header<T> header{};
+			std::vector<uint8_t> body;
+
+			size_t size() const
+			{
+				return sizeof(message_header<T>) + body.size();
+			}
+
+			// Temporary. Makes it easier to output to the console.
+			friend std::ostream& operator << (std::ostream& os, const message<T>* msg)
+			{
+				os << "ID:" << int(msg.header.id) << " Size:" << msg.header.size;
+				return os;
+			}
+		};
 	}
 }
